070a23079c990c4b33329abf665dcc38f05ce7d2
David John Gagne <djgagne@ou.edu>
2020-09-10 16:28:20 -0600

    Added gitignore and skeleton of other directories

---
+++ README.md
@@ -0,0 +1,14 @@
+# Tornado JTTI
+
+## Requirements
+* numpy
+* scipy
+* matplotlib
+* tensorflow
+* scikit-learn
+* xarray
+
+## Install
+Run the following command to install tornadojtti into your python environment:
+`pip install .`
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import os
+
+if __name__ == "__main__":
+    setup(name="tornadojtti",
+          version="0.1",
+          description="Tornado prediction with deep learning",
+          author="University of Oklahoma/University Corporation for Atmospheric Research",
+          author_email="nsnook@ou.edu",
+          url="https://github.com/ai2es/tornado_jtti",
+          packages=["tornadojtti"],
+          license="CC0",
+         )
