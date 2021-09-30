b0fb03deee427e51df59feb9b08eab7c7af7c4de
djgagne@ou.edu <davidjohngagne@gmail.com>
2021-09-30 14:57:25 -0600

    Added storm id matching script

---
+++ scripts/match_ids.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+import xarray as xr
+from os.path import exists, join
+from os import listdir
+from glob import glob
+import pickle
+
+def main():
+    tor_path = "/glade/p/cisl/aiml/jtti_tornado/gridrad_storm_tracks/"
+    all_dates = get_all_pickle_dates(tor_path)
+    print(all_dates)
+    for date in all_dates:
+        storm_ds = load_patch_day(tor_path, date)
+        if storm_ds is None:
+            continue
+        storm_poly = load_track_day(tor_path, date)
+        poly_id = storm_poly["full_id_string"] + "_" + storm_poly["valid_time_unix_sec"].astype(str)
+        ds_id = pd.Series(storm_ds["full_storm_id_strings"].values).str.decode("utf-8") + "_" + pd.Series(storm_ds["storm_times_unix_sec"].values).astype(str)
+        overlap = np.intersect1d(poly_id, ds_id)
+        print(date, poly_id.size, ds_id.size, overlap.size)
+        storm_ds.close()
+
+    return
+
+
+def get_all_pickle_dates(tor_path):
+    tor_years = sorted(listdir(tor_path))[:-1]
+    print(tor_years)
+    all_dates = []
+    for year in tor_years:
+        all_dates.extend(sorted(listdir(join(tor_path, year))))
+    return all_dates
+
+def load_track_day(tor_path, date):
+    object_files = sorted(glob(join(tor_path, date[:4], date, "scale_314159265m2", "*.p")))
+    storm_frames = []
+    for object_file in object_files:
+        with open(object_file, "rb") as obj_file_r:
+            storm_frames.append(pickle.load(obj_file_r))
+    storms = pd.concat(storm_frames)
+    return storms
+
+
+def load_patch_day(tor_path, date, downsampled=False):
+    if downsampled:
+        down_dir = "downsampled_2012-2018"
+    else:
+        down_dir = "nondownsampled_2011_2015-2018"
+    tor_file = join(tor_path, "tornado_occurrence", down_dir, "learning_examples", date[:4], f"input_examples_{date}.nc")
+    tor_ds = None
+    if exists(tor_file):
+        tor_ds = xr.open_dataset(tor_file)
+    return tor_ds
+
+if __name__ == "__main__":
+    main()
