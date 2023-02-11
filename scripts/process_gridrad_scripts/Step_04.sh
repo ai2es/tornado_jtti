822f31bd70d6bd4882c017d501b7d64941da3553
Randy Chase <randychase@schooner2.oscer.ou.edu>
2023-02-11 09:40:13 -0600

    check in all processing steps

--- scripts/process_gridrad_scripts/Step_04.sh
+++ scripts/process_gridrad_scripts/Step_04.sh
@@ -3,9 +3,9 @@
 #SBATCH --nodes=1
 #SBATCH -n 2
 #SBATCH --mem=28G
-#SBATCH --time=08:00:00
+#SBATCH --time=05:00:00
 #SBATCH --chdir=/ourdisk/hpc/ai2es/randychase/GewitterGefahr/gewittergefahr/scripts/
-#SBATCH --job-name="Step2c_2013"
+#SBATCH --job-name="Step4_2013"
 #SBATCH --mail-user=randychase@ou.edu
 #SBATCH --mail-type=ALL
 #SBATCH --mail-type=END
@@ -14,15 +14,14 @@
 #SBATCH --array=32%1
 
 #need all yyyymmdd strings  
-mapfile -t SPC_DATE_STRINGS < /home/randychase/orderofprocessing_spc_dates_2013.txt
+mapfile -t SPC_DATE_STRINGS < /ourdisk/hpc/ai2es/tornado/tornado_jtti/scripts/process_gridrad_scripts/orderofprocessing_files/orderofprocessing_spc_dates_2013.txt
 
 #grab the current one that is running from the array param above
 this_spc_date_string=${SPC_DATE_STRINGS[$SLURM_ARRAY_TASK_ID]}
 
 #print some things to the .out for debuging if it fails.
-echo "running echo classification for:"
+echo "running final tracking for:"
 echo "Array index = ${SLURM_ARRAY_TASK_ID} ... SPC date = ${this_spc_date_string}"
-
 
 #source  my python envs
 source ~/.bashrc
@@ -30,8 +29,13 @@ bash
 
 conda activate gewitter 
 
-python ./run_echo_classification.py \
---radar_source_name="gridrad" \
---spc_date_string=${this_spc_date_string} \
---input_radar_dir_name="/ourdisk/hpc/ai2es/tornado/gridrad_gridded_V2" \
---output_dir_name="/ourdisk/hpc/ai2es/tornado/echo_class_V2/"
+python -u ./reanalyze_storm_tracks.py \
+--input_tracking_dir_name="/ourdisk/hpc/ai2es/tornado/prelim_tracking_V2" \
+--first_spc_date_string=${this_spc_date_string} \
+--last_spc_date_string=${this_spc_date_string} \
+--max_velocity_diff_m_s01=30 \
+--max_link_distance_m_s01=30 \
+--max_join_time_seconds=720 \
+--max_join_error_m_s01=30 \
+--min_duration_seconds=1 \
+--output_tracking_dir_name="/ourdisk/hpc/ai2es/tornado/final_tracking_V2/"
--- scripts/process_gridrad_scripts/storm_centered_scripts/Step_08a.sh
+++ scripts/process_gridrad_scripts/storm_centered_scripts/Step_08a.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
-#SBATCH -p normal
+#SBATCH -p ai2es_v100
 #SBATCH --nodes=1
 #SBATCH -n 2
-#SBATCH --mem=28G
-#SBATCH --time=08:00:00
+#SBATCH --mem=4G
+#SBATCH --time=00:30:00
 #SBATCH --chdir=/ourdisk/hpc/ai2es/randychase/GewitterGefahr/gewittergefahr/scripts/
-#SBATCH --job-name="Step2c_2013"
+#SBATCH --job-name="Step8a_2016"
 #SBATCH --mail-user=randychase@ou.edu
 #SBATCH --mail-type=ALL
 #SBATCH --mail-type=END
 #SBATCH --output=/home/randychase/slurmouts/R-%x.%j.out
 #SBATCH --error=/home/randychase/slurmouts/R-%x.%j.err
-#SBATCH --array=32%1
+#SBATCH --array=0-100%2
 
 #need all yyyymmdd strings  
-mapfile -t SPC_DATE_STRINGS < /home/randychase/orderofprocessing_spc_dates_2013.txt
+mapfile -t SPC_DATE_STRINGS < /home/randychase/orderofprocessing_spc_dates_2016.txt
 
 #grab the current one that is running from the array param above
 this_spc_date_string=${SPC_DATE_STRINGS[$SLURM_ARRAY_TASK_ID]}
 
 #print some things to the .out for debuging if it fails.
-echo "running echo classification for:"
+echo "determining worker splits for:"
 echo "Array index = ${SLURM_ARRAY_TASK_ID} ... SPC date = ${this_spc_date_string}"
 
-
-#source  my python envs
-source ~/.bashrc
+#source my python env
+source /home/randychase/.bashrc
 bash 
 
 conda activate gewitter 
 
-python ./run_echo_classification.py \
---radar_source_name="gridrad" \
+#need a fancier way to split up the work. right now, the peak convective time of day overwelms one worker. 
+python /ourdisk/hpc/ai2es/randychase/GewitterGefahr/gewittergefahr/scripts/determine_splits.py \
 --spc_date_string=${this_spc_date_string} \
---input_radar_dir_name="/ourdisk/hpc/ai2es/tornado/gridrad_gridded_V2" \
---output_dir_name="/ourdisk/hpc/ai2es/tornado/echo_class_V2/"
+--input_directory_name='/ourdisk/hpc/ai2es/tornado/conus_only/' \
+--n_splits=75
