e7779b4822c9844141f5b54a5c383db0d119b5a7
Monique Shotande <momoshog@schooner2.oscer.ou.edu>
2023-02-20 19:33:52 -0600

    change hypermodel constructor to require input shape. fixed logrithmic filternum computation and selection. fixed pool and unpool keras.hyperparams. removed linear activation option for now (add back later). default tuner option set to base tuner. data_prep updated to tf dataset. attempt to include unittest for command line args

--- lydia_scripts/scripts_tensorboard/unet_hypermodel_test.py
+++ lydia_scripts/scripts_tensorboard/unet_hypermodel_test.py
@@ -1,9 +1,12 @@
-import unittest
+import sys, unittest, argparse
 import unet_hypermodel as uh
 
 class TestUNetHyperModel(unittest.TestCase):
 
     def test_create_parser(self):
+        argv = sys.argv[1:]
+        args = ['--in_dir', '../../../test_data',
+                '--out_dir', '../../../test_data']
         parser = uh.create_parser()
         self.assertEqual('foo'.upper(), 'FOO')
 
@@ -27,5 +30,14 @@ class TestUNetHyperModel(unittest.TestCase):
         with self.assertRaises(TypeError):
             s.split(2)
 
+def parse_args():
+    parser = uh.create_parser()
+    ns, args = parser.parse_known_args(namespace=unittest)
+    print(ns, args)
+    return ns, sys.argv[:1] + args
+
 if __name__ == '__main__':
+    args, unittest_args = parse_args()   # run this first
+    print(args, unittest_args)
+    sys.argv[:] = unittest_args       # create cleans argv for main()
     unittest.main()
