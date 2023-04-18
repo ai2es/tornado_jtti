f4efa1d63530a00f352a0ec90d7cc0e8fe6fc383
ggantos <ggantos@ucar.edu>
2023-04-18 01:54:41 +0000

    wofs to preds working

--- lydia_scripts/wofs_raw_predictions_azure.sh
+++ lydia_scripts/wofs_raw_predictions_azure.sh
@@ -1,5 +1,5 @@
 WOFS_REL_PATH="2019/20190520/0030/ENS_MEM_1"
-WOFS_FILE="wrfwof_d01_2019-05-21_00:30:00 "
+WOFS_FILE="wrfwof_d01_2019-05-21_00:30:00"
 
 python wofs_raw_predictions_azure.py \
 --loc_wofs="/datadrive/wofs/${WOFS_REL_PATH}/${WOFS_FILE}"  \
@@ -9,7 +9,7 @@ python wofs_raw_predictions_azure.py \
 --dir_figs="/datadrive/wofs_figs/${WOFS_REL_PATH}/${WOFS_FILE}" \
 --with_nans  \
 --fields U WSPD10MAX W_UP_MAX \
---loc_model="/home/ggantos/tornado_jtti/lydia_scripts/models/2023_04_06_18_23_50_hp_model01.h5"  \
+--loc_model="/home/ggantos/tornado_jtti/lydia_scripts/models/initialrun_model8/initialrun_model8.h5"  \
 --file_trainset_stats="/home/ggantos/tornado_jtti/lydia_scripts/training_metadata/3D_light/training_onehot_tor/training_metadata_ZH_only.nc" \
 --write=4 \
 --dry_run
