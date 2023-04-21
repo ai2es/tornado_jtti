624c29f3fa89f71dc53535a6733acb238abe1f87
ggantos <ggantos@ucar.edu>
2023-04-21 09:37:27 -0600

    scripts ready to test all downloadable wofs file

--- real_time_scripts/download_wofs.py
+++ real_time_scripts/download_wofs.py
@@ -1,4 +1,4 @@
-import json, argparse
+import json, argparse, subprocess
 from azure.storage.queue import (
     QueueClient, TextBase64EncodePolicy, TextBase64DecodePolicy
 )
@@ -18,23 +18,40 @@ def test_monitor_queue():
                         message_encode_policy=TextBase64EncodePolicy(),
                         message_decode_policy=TextBase64DecodePolicy())
     while True:
-        # Receive messages one-by-one
-        print('checking for messages...')
-        messages = queue.receive_messages()
         
-        #if len(messages) == 0:
-            #print('no message, sleeping')
-            #continue
+        print('Checking for messages...')
+        msg = queue.receive_message(visibility_timeout=120)
         
-        #else: 
+        if msg == None:
+            print('No message: sleeping.')
+            time.sleep(10)
+            continue
+
         with open('message_log.txt', 'a') as message_log:
-            for message in messages:
-                message_log.write(message.content)
-                body = json.loads(message.content)
-                print('Saving message to storage blob:')
-                for file_string in body['data']:
-                    print(file_string)
-                    
+            message_log.write(msg.content)
+        try: 
+            print('Saving message content to storage blob:')
+            body = json.loads(msg.content)
+            print(f"\t{body}")
+            for file_string in body["data"]:
+                year = file_string.split("WOFSRun")[1][:4]
+                date = file_string.split("WOFSRun")[1].split("-")[0]
+                run_time = file_string.split("/fcst")[0][-4:]
+                mem = file_string.split("fcst/mem")[1].split("/wrfwof")[0]
+                filename = file_string.split("?se=")[0].rsplit('/', 1)[1]
+                path = f"/datadrive/wofs/{year}/{date}/{run_time}/ENS_MEM_{mem}/"
+                
+                os.makedirs(path, exist_ok = True)
+                subprocess.run(["azcopy",
+                                "copy",
+                                f"{file_string}",
+                                f"{path}{filename}"])
+                
+            time.sleep(10)
+            queue.delete_message(msg)
+            
+        except Exception as e:
+            print(f'ERROR: {e}')
         
 if __name__ == '__main__':
     test_monitor_queue()
