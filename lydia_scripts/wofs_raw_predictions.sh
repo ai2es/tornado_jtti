6b76b072a7c3d663dfa38dd8c9bfec9a7f22a2ea
Monique Shotande <momoshog@schooner1.oscer.ou.edu>
2023-04-22 10:09:37 -0500

    clean up wofs raw preds script

--- lydia_scripts/wofs_raw_predictions.sh
+++ lydia_scripts/wofs_raw_predictions.sh
@@ -31,7 +31,7 @@ WOFS_FILE="wrfwof_d01_2019-04-30_19:35:00"
 #WOFS_FILE="wrfwof_d01_2019-05-18_03:15:00"
 
 python lydia_scripts/wofs_raw_predictions.py \
---dir_preds="/ourdisk/hpc/ai2es/wofs/${WOFS_REL_PATH}/${WOFS_FILE}"  \
+--loc_wofs="/ourdisk/hpc/ai2es/wofs/${WOFS_REL_PATH}/${WOFS_FILE}"  \
 --datetime_format="%Y-%m-%d_%H:%M:%S"  \
 --dir_preds="/ourdisk/hpc/ai2es/momoshog/Tornado/tornado_jtti/wofs_preds/${WOFS_REL_PATH}"  \
 --dir_patches="/ourdisk/hpc/ai2es/momoshog/Tornado/tornado_jtti/wofs_patches/${WOFS_REL_PATH}" \
@@ -45,7 +45,6 @@ python lydia_scripts/wofs_raw_predictions.py \
 load_weights_hps \
 --hp_path="/ourdisk/hpc/ai2es/momoshog/Tornado/tornado_jtti/unet/ZH_only/tuning/tornado_unet_hyper/2023_04_06_18_23_50_hps.csv" \
 --hp_idx=1
-#fields UP_HELI_MAX U U10 V V10
 
 #--loc_model="/ourdisk/hpc/ai2es/tornado/unet/ZH_only/initialrun_model2/initialrun_model2.h5" \
 #--file_trainset_stats="/ourdisk/hpc/ai2es/tornado/learning_patches/tensorflow/3D_light/training_onehot_nontor_tor/training_metadata_ZH_only.nc" \
@@ -56,9 +55,7 @@ load_weights_hps \
 #--hp_path="/ourdisk/hpc/ai2es/momoshog/Tornado/tornado_jtti/unet/ZH_only/tuning/tornado_unet_hyper/hps_2023_03_21_05_03_06.csv" \
 
 
-#--loc_model="/ourdisk/hpc/ai2es/tornado/unet/ZH_only/initialrun_model8/initialrun_model8.h5" \
-#--file_trainset_stats="/ourdisk/hpc/ai2es/tornado/learning_patches/tensorflow/3D_light/training_onehot_nontor_tor/training_metadata_ZH_only.nc"
-
+#fields UP_HELI_MAX U U10 V V10
 #YYYY-MM-DD_hh:mm:ss
 #--file_trainset_stats="/ourdisk/hpc/ai2es/tornado/learning_patches/tensorflow/3D_light/training_onehot_tor/training_metadata_ZH_only.nc"
 
