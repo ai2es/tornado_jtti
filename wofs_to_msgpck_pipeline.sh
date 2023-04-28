1e2dd6409d7226e5abd81fcaab640ca71b77c60d
ggantos <ggantos@ucar.edu>
2023-04-28 19:43:44 +0000

    error in wofs_to_preds.py

--- wofs_to_msgpck_pipeline.sh
+++ wofs_to_msgpck_pipeline.sh
@@ -2,15 +2,16 @@ azcopy login --identity
 
 BLOB_URL_NCAR="https://wofsdltornado.blob.core.windows.net"
 ACCOUNT_URL_NCAR="https://wofsdltornado.queue.core.windows.net/?sv=2021-12-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-06-15T15:00:00Z&st=2023-04-26T15:00:00Z&spr=https&sig=H2JOkeMn0UuhOqyuifMc%2BCfoSXoN5ZRL7mCe9iGEjBM%3D"
-QUEUE_NAME_NCAR_WOFS_TO_PREDS="queue-wofs-to-preds"
+#QUEUE_NAME_NCAR_WOFS_TO_PREDS="queue-wofs-to-preds"
+QUEUE_NAME_NCAR_WOFS_TO_PREDS="wrf-wofs-queue"
 QUEUE_NAME_NCAR_PREDS_TO_MSGPK="queue-preds-to-msgpk"
 
-python real_time_scripts/download_wofs.py  \
---account_url_wofs='https://storwofstest003.queue.core.windows.net/?sv=2019-02-02&st=2023-03-09T20%3A39%3A15Z&se=2024-01-01T06%3A00%3A00Z&sp=rp&sig=biy6JZg2n4Wmg%2BLHF4QnVLQvt%2F4W8oYJhXMiaTkyj4U%3D'  \
---queue_name_wofs='wofs-ucar'  \
---blob_url_ncar=$BLOB_URL_NCAR  \
---account_url_ncar=$ACCOUNT_URL_NCAR  \
---queue_name_ncar_wofs_to_preds=$QUEUE_NAME_NCAR_WOFS_TO_PREDS
+#python real_time_scripts/download_wofs.py  \
+#--account_url_wofs='https://storwofstest003.queue.core.windows.net/?sv=2019-02-02&st=2023-03-09T20%3A39%3A15Z&se=2024-01-01T06%3A00%3A00Z&sp=rp&sig=biy6JZg2n4Wmg%2BLHF4QnVLQvt%2F4W8oYJhXMiaTkyj4U%3D'  \
+#--queue_name_wofs='wofs-ucar'  \
+#--blob_url_ncar=$BLOB_URL_NCAR  \
+#--account_url_ncar=$ACCOUNT_URL_NCAR  \
+#--queue_name_ncar_wofs_to_preds=$QUEUE_NAME_NCAR_WOFS_TO_PREDS
 
 python real_time_scripts/wofs_to_preds.py  \
 --account_url_ncar=$ACCOUNT_URL_NCAR  \
@@ -28,8 +29,8 @@ python real_time_scripts/wofs_to_preds.py  \
 --file_trainset_stats="lydia_scripts/training_metadata/3D_light/training_onehot_tor/training_metadata_ZH_only.nc" \
 --write=4
 
-python real_time_scripts/preds_to_msgpk.py \
---account_url_ncar=$ACCOUNT_URL_NCAR  \
---queue_name_ncar_preds_to_msgpk=$QUEUE_NAME_NCAR_PREDS_TO_MSGPK  \
---variable="ML_PREDICTED_TOR" \
---threshold=0.08
+#python real_time_scripts/preds_to_msgpk.py \
+#--account_url_ncar=$ACCOUNT_URL_NCAR  \
+#--queue_name_ncar_preds_to_msgpk=$QUEUE_NAME_NCAR_PREDS_TO_MSGPK  \
+#--variable="ML_PREDICTED_TOR" \
+#--threshold=0.08
