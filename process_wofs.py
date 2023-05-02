6566cd405f74cf63cde05958eb5b39031cfcf829
ggantos <ggantos@ucar.edu>
2023-05-02 10:17:58 -0600

    ready for full pipeline test

--- process_wofs.py
+++ process_wofs.py
@@ -1,10 +1,11 @@
-import json, time, argparse, traceback
+import json, time, argparse, traceback, glob
 from azure.storage.queue import QueueClient, TextBase64EncodePolicy, TextBase64DecodePolicy
-from multiprocessing.pool import Pool, ThreadPool
+from multiprocessing.pool import Pool
+from real_time_scripts import preds_to_msgpk
 
 
 def process_one_file(wofs_filepath, args):
-    from real_time_scripts import download_file, wofs_to_preds, preds_to_msgpk
+    from real_time_scripts import download_file, wofs_to_preds
     ncar_filepath = download_file.download_file(wofs_filepath, args)
     vm_filepath = wofs_to_preds.wofs_to_preds(ncar_filepath, args)
 
@@ -43,8 +44,8 @@ def parse_args():
                         help='Shape of patches. Can be empty (), 2D (xy,), 2D (x, y), or 3D (x, y, h) tuple. If empty tuple, patching is not performed. If tuple length is 1, the x and y dimension are the same. Ex: (32) or (32, 32).')
     parser.add_argument('--with_nans', action='store_true', 
         help='Set flag such that data points with reflectivity=0 are stored as NaNs. Otherwise store as normal floats. It is recommended to set this flag')
-    parser.add_argument('-Z', '--ZH_only', action='store_true',
-        help='Use flag to only extract the reflectivity (COMPOSITE_REFL_10CM and REFL_10CM), updraft (UP_HELI_MAX) and forecast time (Times) data fields, excluding divergence and vorticity fields. Regardless of the value of this flag, only reflectivity is used for training and prediction.')
+    parser.add_argument('-Z', '--ZH_only', action='store_true',  
+        help='Use flag to only extract the reflectivity (COMPOSITE_REFL_10CM and REFL_10CM), updraft (UP_HELI_MAX) and forecast time (Times) data fields, excluding divergence and vorticity fields. Additionally, do not compute divergence and vorticity. Regardless of the value of this flag, only reflectivity is used for training and prediction.')
     parser.add_argument('-f', '--fields', type=str, nargs='+',
         help='Space delimited list of additional WoFS fields to store. Regardless of whether these fields are specified, only reflectivity is used for training and prediction. Ex use: --fields U WSPD10MAX W_UP_MAX')
 
@@ -79,6 +80,7 @@ if __name__ == '__main__':
                              queue_name=args.queue_name_wofs,
                              message_encode_policy=TextBase64EncodePolicy(),
                              message_decode_policy=TextBase64DecodePolicy())
+    
     with Pool(4, maxtasksperchild=1) as p:
         while True:
             msg = queue_wofs.receive_message(visibility_timeout=5*60)
@@ -86,14 +88,33 @@ if __name__ == '__main__':
             if msg == None:
                 print('No message: sleeping.')
                 time.sleep(10)
+                
+                # start the msgpk process if enough files have been saved
+                files = sorted(glob.glob(os.path.join(path_preds, f"ENS_MEM_**", "wrfwof_d01_*")))
+                if path_preds[-2:] == "00":
+                    if len(files) == 1314:
+                        preds_to_msgpk.preds_to_msgpk(path_preds, args)
+                    else:
+                        continue
+                if path_preds[-2:] == "30":
+                    if len(files) == 666:
+                        preds_to_msgpk.preds_to_msgpk(path_preds, args)
+                    else:
+                        continue
                 continue
 
             files = json.loads(msg.content)["data"]
             try:
-                p.starmap_async(process_one_file, [(wofs_filepath, args) for wofs_filepath in files])
+                #p.starmap_async(process_one_file, [(wofs_filepath, args) for wofs_filepath in files])
+                p.starmap(process_one_file, [(wofs_filepath, args) for wofs_filepath in files])
             except Exception as e:
                 print(traceback.format_exc())
                 raise e
+            
+            path_preds = os.path.join("/datadrive2/wofs-preds/2023/", msg.content["runtime"][:8], msg.content["runtime"][-4:])
+            with open('230502_messages.txt', 'a') as a_writer:
+                a_writer.write(msg)
             queue_wofs.delete_message(msg)
+        
         p.close()
         p.join()
--- real_time_scripts/preds_to_msgpk.py
+++ real_time_scripts/preds_to_msgpk.py
@@ -9,7 +9,7 @@ import argparse
 from azure.storage.queue import QueueClient, TextBase64EncodePolicy, TextBase64DecodePolicy
 
 
-def preds_to_msgpk(args):
+def preds_to_msgpk(path_preds, args):
 
     def get_sparse_dict(ds):
         """ Convert variable from xarray dataset to a compressed sparse row matrix using a specified threshold."""
@@ -22,22 +22,20 @@ def preds_to_msgpk(args):
 
         return dict(rows=rows.astype('uint16').tolist(), columns=columns.astype('uint16').tolist(), values=probs)
     
-    path_preds = vm_filepath.split('ENS')[0]
     path_preds_msgpk = path_preds.replace('wofs-preds', 'wofs-preds-msgpk')
     path_preds_msgpk = path_preds_msgpk[:-6] + path_preds_msgpk[-5:]
     os.makedirs(path_preds_msgpk, exist_ok=True)
-    filename = vm_filepath.rsplit('/', 1)[1]
     
     ds_list = []
     for i in range(1, 19):
-        files = sorted(glob.glob(os.path.join(path_preds, f"ENS_MEM_{i}", filename)))
-        ds_list.append(xr.open_mfdataset(files, concat_dim='Time', combine='nested')[args.variable])
+        files = sorted(glob.glob(os.path.join(path_preds, f"ENS_MEM_{i}", "wrfwof_d01_*")))
+        ds_list.append(xr.open_mfdataset(files, concat_dim='Time', combine='nested', decode_times=False)[args.variable])
     ds_all = xr.concat(ds_list, dim='member')
     ds_mean = ds_all.mean(dim='member').load()
     ds_median = ds_all.median(dim='member').load()
     ds_max = ds_all.max(dim='member').load()
 
-    files = sorted(glob.glob(os.path.join(args.dir_preds, f"ENS_MEM_1", "wrfwof_d01_*")))
+    files = sorted(glob.glob(os.path.join(path_preds, f"ENS_MEM_1", "wrfwof_d01_*")))
     for timestep, f in enumerate(files):
         data = {}
         datetime = f[-19:-2].replace("-", "").replace(":", "").replace("_", "")
@@ -55,7 +53,7 @@ def preds_to_msgpk(args):
         data["MEM_median"] = median_sparse_dict
         se_coords = [ds['XLONG'][0, 0, 0].values.tolist(), ds['XLAT'][0, 0, 0].values.tolist()]
         data['se_coords'] = se_coords
-        with open(os.path.join(args.dir_preds_msgpk, f"wofs_sparse_prob_{datetime}.msgpk"), 'wb') as outfile:
+        with open(os.path.join(path_preds_msgpk, f"wofs_sparse_prob_{datetime}.msgpk"), 'wb') as outfile:
             packed = msgpack.packb(data)
             outfile.write(packed)
             print(f"Saving {outfile}")
