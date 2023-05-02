eb32872b849664f7bb2a138f092179ab3d7d3a85
Monique Shotande <momoshog@schooner1.oscer.ou.edu>
2023-05-02 00:56:57 -0500

    include dry run flag and debug output

--- lydia_scripts/scripts_data_pipeline/save_light_model_patches.sh
+++ lydia_scripts/scripts_data_pipeline/save_light_model_patches.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 #SBATCH --partition=normal
 #SBATCH --nodes=1
-#SBATCH -n 20
-#SBATCH --ntasks=20
+#SBATCH --ntasks=20 ##-n 20
 #SBATCH --mem=10G
-#SBATCH --time=10:30:00
-#SBATCH --job-name=save_light_data
-#SBATCH --mail-user=lydiaks2@illinois.edu
+#SBATCH --time=1:30:00
+#SBATCH --job-name=test__save_light_data
+#SBATCH --mail-user=monique.shotande@ou.edu
 #SBATCH --mail-type=ALL
-#SBATCH --output=/home/lydiaks2/tornado_project/output/R-%x.%j.out
-#SBATCH --error=/home/lydiaks2/tornado_project/output/R-%x.%j.err
+#SBATCH --chdir=/home/momoshog/Tornado/tornado_jtti
+#SBATCH --output=/home/momoshog/Tornado/slurm_out/tornado_jtti/%x_%j.out
+#SBATCH --error=/home/momoshog/Tornado/slurm_out/tornado_jtti/%x_%j.err
+###############################################################
 
-#source my python env
-source /home/lydiaks2/.bashrc
+
+# Source conda env
+#source /home/lydiaks2/.bashrc
+source ~/.bashrc
 bash 
+# conda activate hagelslag
 
-#conda activate hagelslag
-python -u /home/lydiaks2/tornado_project/scripts_data_pipeline/save_light_model_patches.py \
+python -u lydia_scripts/scripts_data_pipeline/save_light_model_patches.py \
 --input_xarray_path="/ourdisk/hpc/ai2es/tornado/learning_patches_V2/xarray/3D/size_32/forecast_window_5/" \
---output_path="/ourdisk/hpc/ai2es/tornado/learning_patches_V2/xarray/3D_light/size_32/forecast_window_5/"
+--output_path="/ourdisk/hpc/ai2es/tornado/learning_patches_V2/xarray/3D_light/size_32/forecast_window_5/" \
+--dry_run
+
+#python -u /home/lydiaks2/tornado_project/scripts_data_pipeline/save_light_model_patches.py \
+#--input_xarray_path="/ourdisk/hpc/ai2es/tornado/learning_patches_V2/xarray/3D/size_32/forecast_window_5/" \
+#--output_path="/ourdisk/hpc/ai2es/tornado/learning_patches_V2/xarray/3D_light/size_32/forecast_window_5/"
