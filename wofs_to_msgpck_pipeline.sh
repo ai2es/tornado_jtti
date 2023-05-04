d2e627ed0b7bf6819afbc0efce4e15f7ca7210cb
ggantos <ggantos@ucar.edu>
2023-05-04 20:38:56 +0000

    changes from VM

--- wofs_to_msgpck_pipeline.sh
+++ wofs_to_msgpck_pipeline.sh
@@ -25,5 +25,3 @@ load_weights_hps  \
 --hp_idx=1  \
 |& tee ./logs/$(date +"%Y%m%d_%H%M%S")_output.txt
 
-#--loc_model="lydia_scripts/models/initialrun_model8/initialrun_model8.h5"  \
-#--file_trainset_stats="lydia_scripts/training_metadata/3D_light/training_onehot_tor/training_metadata_ZH_only.nc" \
